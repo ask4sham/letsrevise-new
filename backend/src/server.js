2d3209e772a7440f69c17917b0043eda1c78d235
ask4sham <ask4sham@yahoo.co.uk>
2025-12-23 22:24:44 +0000

    Step 1: Created backend/src structure and new server bootstrap

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log('Backend running on port ' + PORT);
+});
