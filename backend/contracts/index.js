12589dc3961b006adbc33a5d398a48124057e7d4
ask4sham <ask4sham@yahoo.co.uk>
2026-02-04 09:48:44 +0000

    Add aiGenerationJobErrors to contract exports

--- backend/contracts/index.js
+++ backend/contracts/index.js
@@ -1,11 +1,13 @@
 const aiGenerationJobContract = require("./aiGenerationJobContract");
 const aiGenerationJobPolicy = require("./aiGenerationJobPolicy");
 const aiGenerationJobTypeSpecs = require("./aiGenerationJobTypeSpecs");
+const aiGenerationJobErrors = require("./aiGenerationJobErrors");
 
 module.exports = {
   aiGenerationJobContract,
   aiGenerationJobPolicy,
   aiGenerationJobTypeSpecs,
+  aiGenerationJobErrors,
 };
 
 
